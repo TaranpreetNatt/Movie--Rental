af57302b98130698d4625baa136ddfd8e32f0c5d
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-08-18 02:27:51 -0700

    added a route to the customers api

--- vidly/index.js
+++ vidly/index.js
@@ -1,11 +1,17 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDb...'))
+  .catch((err) => console.log('Could not connect to MongoDb', err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
