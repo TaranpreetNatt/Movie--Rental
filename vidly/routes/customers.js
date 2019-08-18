da10771a22582981c6af7ddb315ec05d091e95d8
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-08-18 02:31:24 -0700

    Created an endpoint for the customers

---
+++ vidly/routes/customers.js
@@ -0,0 +1,70 @@
+const { Customer, validate} = require('../models/customer');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let customer = new Customer({
+    isGold: req.body.isGold,
+    name: req.body.name,
+    phone: req.body.phone
+  });
+
+  customer = await customer.save()
+    .catch((err) => {
+      res.status(500).send('Could not save to database');
+      console.log("Error", err.message);
+    });
+  res.send(customer);
+});
+
+router.get('/', async (req, res) => {
+  const customers = await Customer
+    .find()
+    .sort('name');
+  res.send(customers);
+});
+
+router.get('/:id', async (req, res) => {
+  const customer = await Customer
+    .findById(req.params.id)
+    .catch((err) => {
+      res.status(404).send('Customer with the given ID was not found');
+      console.log("Error", err.message);
+    });
+  res.send(customer);
+});
+
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(req.params.id, {
+    isGold: req.body.isGold,
+    name: req.body.name,
+    phone: req.body.phone
+  }, { new: true})
+    .catch((err) => {
+      res.status(500).send('Could not update customer.');
+      console.log('Error', err.message);
+    });
+  
+  res.send(customer);
+});
+
+router.delete('/:id', async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id)
+    .catch((err) => {
+      res.status(404).send('Customer with the given ID was not deleted.');
+      console.log('Error', err.message);
+    });
+  
+  if (!customer) res.status(404).send('Customer with the given ID was not found.');
+
+  res.send(customer);
+});
+
+module.exports = router;
