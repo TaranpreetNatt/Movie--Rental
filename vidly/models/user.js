ea61aecf5805ebf427d125a4ae8cb5b2f77a4679
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-08-20 03:53:20 -0700

    created a schema and validation for the user

---
+++ vidly/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 5,
+    max: 50
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    min: 5,
+    max: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 1024
+  }
+});
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).max(255).required()
+  };
+  return Joi.validate(user, schema);
+}
+
+module.exports.userSchema = userSchema;
+module.exports.User = User;
+module.exports.validateUser = validateUser;
