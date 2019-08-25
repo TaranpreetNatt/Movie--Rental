43d41acf774591beba3a20425133ae4abae66749
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-08-25 04:54:18 -0700

    File for testing lib.js

---
+++ testing-demo/tests/lib.test.js
@@ -0,0 +1,58 @@
+const lib = require('../lib');
+
+describe('absolute', () => {
+  it('should return a positive number if input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1); // to be compares the memory locations as well
+  });
+
+  it('should return a positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it('should return a 0 number if input is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+});
+
+describe('greet', () => {
+  it('should return the greeting message', () => {
+    const result = lib.greet('Mosh');
+    expect(result).toMatch(/Mosh/);
+    // expect(result).toContain('Mosh'); // if you don't want to use regular expressions
+  });
+});
+
+describe('getCurrencies', () => {
+  it('should return supported currencies', () => {
+    const result = lib.getCurrencies();
+
+    // Too general
+    expect(result).toBeDefined();
+    expect(result).not.toBeNull();
+
+    // Too specific
+    expect(result[0]).toBe('USD');
+    expect(result[1]).toBe('AUD');
+    expect(result[2]).toBe('EUR');
+    expect(result.length).toBe(3);
+
+    // Proper way
+    expect(result).toContain('USD');
+    expect(result).toContain('AUD');
+    expect(result).toContain('EUR');
+
+    // Ideal way
+    expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']));
+  });
+});
+
+describe('getProduct', () => {
+  it('should return the product with the given id', () => {
+    const result = lib.getProduct(1);
+    // expect(result).toEqual({id: 1, price: 10}); // This tests the object has exactly these two properties
+    expect(result).toMatchObject({id:1, price: 10});  // This test checks if the object contains id: 1, price:10
+  })
+});
