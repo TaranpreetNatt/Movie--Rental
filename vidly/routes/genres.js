84f6e4206fc11f1aa1736e42e849632d34526dbc
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-09-01 21:18:53 -0700

    some little additions and removals

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,36 +1,32 @@
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const validateObjectId = require('../middleware/validateObjectId');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const { Genre, validate} = require('../models/genre');
 
 router.get('/', async (req, res) => {
-  throw new Error('Could not get the genres.');
   const genres = await Genre
     .find()
     .sort('name');
   res.send(genres);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id)
-    .catch( (err) => {
-      res.status(404).send('The genre with the given ID was not found.');
-      // console.log(err.message);
-    });
+  
+  if(!genre) return res.status(404).send('The genre with the given ID was not found.');
+
   res.send(genre);
 });
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', [auth, validateObjectId], async (req, res) => {
   const { error } = validate(req.body);
   if(error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true})
-    .catch((err) => {
-      res.status(404).send('Could not update genre.');
-      // console.log('Error', err.message);
-    });
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true});
+  if(!genre) return res.status(404).send('The genre with the given ID was not found.');
   res.send(genre);
 });
 
@@ -40,20 +36,13 @@ router.post('/', auth, async (req, res) => {
 
   const genre = new Genre({ name: req.body.name });
   const result = await genre.save()
-    .catch((err) => {
-      res.status(500).send('Could not save genre');
-      // console.log('Error', err.message);
-    });
-
+  
   res.send(result);
 });
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [auth, admin, validateObjectId], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id)
-    .catch((err) => {
-      res.status(404).send("The genre with the given ID does not exist");
-      // console.log('Error', err.message);
-    });
+  if(!genre) return res.status(404).send("The genre with the given ID does not exist");
   res.send(genre);
 });
 
--- vidly/startup/logging.js
+++ vidly/startup/logging.js
@@ -1,27 +1,28 @@
-require('express-async-errors');
 const winston = require('winston');
+require('express-async-errors');
 require('winston-mongodb');
 
 module.exports = function() {
-  process.on('uncaughtException', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
-  });
 
-  // Doesn't work for winston-mongodb@5.0.0
+  // // This is how winston.handleExceptions() works internally
+  // process.on('uncaughtException', (ex) => {
+  //   winston.error(ex.message, ex);
+  //   process.exit(1);
+  // });
+
+  //// Doesn't work for winston-mongodb@5.0.0
   winston.handleExceptions(
     new winston.transports.Console({ colorize: true, prettyPrint: true}),
     new winston.transports.File({ filename: 'uncaughtExceptions.log'})
-    );
+  );
 
   process.on('unhandledPromiseRejection', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
+    throw ex;
   });
 
   winston.add(winston.transports.File, { filename: 'logfile.log'});
   winston.add(winston.transports.MongoDB,{ 
     db: 'mongodb://localhost/vidly',
     level: 'info',
-    });
+  });
 }
