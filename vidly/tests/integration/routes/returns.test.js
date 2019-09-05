3c94b8619703cbab65c1001cc263dd2645b6f4f5
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-09-05 14:22:38 -0700

    added test for checking the return of the res object

--- vidly/tests/integration/routes/returns.test.js
+++ vidly/tests/integration/routes/returns.test.js
@@ -1,5 +1,7 @@
 const { Rental } = require('../../../models/rental');
 const { User } = require('../../../models/user');
+const { Movie } = require('../../../models/movie');
+const moment = require('moment');
 const mongoose = require('mongoose');
 const request = require('supertest');
 
@@ -8,12 +10,23 @@ describe('/api/returns', () => {
   let customerId;
   let rental;
   let token;
+  let movie;
 
   beforeEach(async () => {
     server = require('../../../index');
     customerId = mongoose.Types.ObjectId();
     movieId = mongoose.Types.ObjectId();
     token = new User().generateAuthToken();
+
+    movie = new Movie({
+      title: '12345',
+      _id: movieId,
+      dailyRentalRate: 2,
+      genre: { name: '12345' },
+      numberInStock: 10,
+    });
+
+    await movie.save();
 
     rental = new Rental({
       customer: {
@@ -30,17 +43,18 @@ describe('/api/returns', () => {
     await rental.save();
   });
 
+  afterEach(async () => {
+    await rental.remove({});
+    await movie.remove({});
+    await server.close();
+  });
+
   const exec = async () => {
     return await request(server)
       .post('/api/returns')
       .set('x-auth-token', token)
       .send({ customerId, movieId});
   }
-
-  afterEach(async () => {
-    await rental.remove();
-    await server.close();
-  });
 
   it('should return 401 if client is not logged in', async () => {
     token = '';
@@ -81,10 +95,35 @@ describe('/api/returns', () => {
 
   it('should set the return date if input is valid', async () => {
     const res = await exec();
-    const rentalInDb = Rental.findById(rental._id);
-    console.log('rental', rentalInDb.customer.name);
+    const rentalInDb = await Rental.findById(rental._id);
     const diff = new Date() - rentalInDb.dateReturned;
     expect(diff).toBeLessThan(10000); // want difference to be less than 10 seconds
   });
 
+  it('should set the rental fee if rental is valid', async () => {
+    rental.dateOut = moment().add(-7, 'days').toDate();
+    await rental.save();
+
+    const res = await exec();
+    const rentalInDb = await Rental.findById(rental._id);
+
+    expect(rentalInDb.rentalFee).toBe(14);
+  });
+
+  it('should increase the stock of the movie if rental is valid', async () => {
+    const res = await exec();
+    const movieInDb = await Movie.findById(movie._id);
+    expect(movieInDb.numberInStock).toBe(movie.numberInStock + 1);
+  });
+
+  it('should return the rental in the body of the response', async () => {
+    const res = await exec();
+    expect(Object.keys(res.body)).toEqual(expect.arrayContaining([
+      'customer', 'movie', 'dateOut', 'dateReturned', 'rentalFee'
+    ]));
+  });
+
 });
+
+// increase stock: return movie -> increase stock
+// check if dateReturned is not null then increase stock
