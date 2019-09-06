5d7e08c2e2b0d02c4c9221055cbb0caff8d8f029
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-09-06 00:05:12 -0700

    refactored code and created a function to validate user inputs

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -1,38 +1,38 @@
-const moment = require('moment');
+const Joi = require('joi');
 const mongoose = require('mongoose');
 const express = require('express');
-const auth = require('../middleware/auth');
 const router = express.Router();
+const validate = require('../middleware/validate');
+const auth = require('../middleware/auth');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
 
-router.post('/', auth, async (req, res) => {
-  if(!req.body.customerId) return res.status(400).send('customerId was not provided');
-  if(!req.body.movieId) return res.status(400).send('movieId was not provided');
+
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
+
+  let rental = await Rental.lookup(req.body.customerId, req.body.movieId);
   
-  let rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId,
-  });
   if(!rental) return res.status(404).send('Rental not found.');
 
   if(rental.dateReturned) return res.status(400).send('Rental already processed.');
   
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, 'days');
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
   
   await Movie.update({ _id: rental.movie._id }, {
     $inc: { numberInStock: 1 },
   });
 
-
-  return res.status(200).send(rental);
+  return res.send(rental);
 });
 
-
-
+function validateReturn(req){
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
+  }
+  return Joi.validate(req, schema);
+}
 
 
 
